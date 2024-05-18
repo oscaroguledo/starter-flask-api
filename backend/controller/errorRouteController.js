6dd832b23d832ba6e8b5c4e0461497d735cacb2d
Oscar Oguledo <oscarchiagoziem@gmail.com>
2024-05-18 12:22:36 +0000

    updated

--- backend/controller/errorRouteController.js
+++
@@ -1,9 +0,0 @@
-const { generateDefaultResponseObject } = require("../utils/defaultResponseObject");
-
-// 404 controller handler
-exports.handle_404_requests = (req, res, next) => {
-    res.status(404).send(generateDefaultResponseObject({
-        "success": false,
-        "message": "Requested resource unavailable!"
-    }));
-}
--- backend/controller/statusController.js
+++
@@ -1,8 +0,0 @@
-const { generateDefaultResponseObject } = require("../utils/defaultResponseObject")
-
-exports.get_api_status = (req, res) => {
-    return res.status(200).json(generateDefaultResponseObject({ 
-        success: true, 
-        message: 'API is up and running',
-    }))
-}
